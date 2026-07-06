ef172569a26008640ea30d696df55fd7eb4e6e85
Guilherme Souza Almeida <guilherme.almeida.tech@gmail.com>
2026-07-06 17:22:18 -0300

    feat: tratamento de erros

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,16 @@
 import express, { type Express, type Request, type Response } from 'express';
+import { errorLog } from './middlewares/error-log.ts';
+import { errorHandler } from './middlewares/error-handler.ts';
+import { authRoute } from './routes/auth-routes.ts';
 
 const app: Express = express();
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
 });
+
+//Middlewares
+app.use(errorLog);
+app.use(errorHandler);
 
 export default app;
