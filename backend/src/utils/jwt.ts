4f2a2e4f2d7605f1ebdaf7a37305e9fa19895280
Guilherme Souza Almeida <guilherme.almeida.tech@gmail.com>
2026-07-13 19:59:48 -0300

    feat: configurando midleware de autenticacao

--- backend/src/utils/jwt.ts
+++ backend/src/utils/jwt.ts
@@ -1,5 +1,23 @@
-import jwt from "jsonwebtoken";
+import jwt, { JwtPayload } from "jsonwebtoken";
 import { ENVIROMENTS } from "../env-config.ts";
+import { AuthPayload } from "../types/auth.ts";
+
 export async function jwtGenerateToken(object: any): Promise<string> {
     return jwt.sign(object, ENVIROMENTS.secretesKeys.jwtSecreteKey);
+}
+
+export async function jwtVerify(token: string) {
+    const payload = jwt.verify(token, ENVIROMENTS.secretesKeys.jwtSecreteKey) as JwtPayload;
+    return {
+        id: payload.id,
+        email: payload.email
+    }
+}
+
+export async function jwtdecode(token: string): Promise<AuthPayload> {
+    const payload = jwt.decode(token) as AuthPayload;
+    return {
+        id: payload.id,
+        email: payload.email
+    }
 }
